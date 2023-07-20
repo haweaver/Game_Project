a7a1f15cada5efd24df25f9bb7db4198ee1db28a
haweaver <jaymax580@gmail.com>
2023-07-20 11:24:49 -0500

    Game Class Created - Not Started

---
+++ src/Games/Game.java
@@ -0,0 +1,17 @@
+/* 
+ * This class will complete all of the functions that every game must complete
+ */
+
+package Games;
+
+public class Game {
+	
+	//This is the Master Version
+	public void master() {
+		
+		
+	}
+	
+	
+	
+}
